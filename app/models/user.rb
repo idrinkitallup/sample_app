113385a5913d521a8da02dfc292f3f67c6cd92e9
Geoff Walmsley <mathsboy@gmail.com>
2011-12-30 21:48:13 +1300

    added user model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,15 @@
 
 class User < ActiveRecord::Base
   attr_accessible :name, :email
+  
+  email_regex = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i 
+  
+  validates :name, :presence => true,
+                    :length => { :maximum => 50 }
+  validates :email, :presence => true,
+                    :format => { :with => email_regex },
+                    :uniqueness => { :case_sensitive => false }
+  
+
+  
 end
