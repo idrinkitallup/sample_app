cd30a9a70559f0b731d49644445cadd0d8e2d6fd
Geoff Walmsley <mathsboy@gmail.com>
2011-12-31 15:13:11 +1300

    added password and encryption

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,19 @@
-# == Schema Information
-#
-# Table name: users
-#
-#  id         :integer         not null, primary key
-#  name       :string(255)
-#  email      :string(255)
-#  created_at :datetime
-#  updated_at :datetime
-#
-
 class User < ActiveRecord::Base
-  attr_accessible :name, :email
+  
+  # == Schema Information
+  #
+  # Table name: users
+  #
+  #  id                 :integer         not null, primary key
+  #  name               :string(255)
+  #  email              :string(255)
+  #  created_at         :datetime
+  #  updated_at         :datetime
+  #  encrypted_password :string(255)
+  #  
+  
+  attr_accessor :password
+  attr_accessible :name, :email, :password, :password_confirmation
   
   email_regex = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i 
   
@@ -19,7 +22,13 @@ class User < ActiveRecord::Base
   validates :email, :presence => true,
                     :format => { :with => email_regex },
                     :uniqueness => { :case_sensitive => false }
+                    
+  validates :password, :presence => true,
+                       :confirmation => true,
+                       :length => { :within => 6..40 }
+
   
 
   
 end
+
