1aaf16336f9c48e7de901a0c77165aa1b67516c4
Geoff Walmsley <mathsboy@gmail.com>
2012-01-02 15:12:49 +1300

    admin delete powers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
   
-  before_filter :authenticate, :only => [:index, :edit, :update]
+  before_filter :authenticate, :only => [:index, :edit, :update, :destroy]
   before_filter :correct_user, :only => [:edit, :update]
+  before_filter :admin_user,   :only => :destroy
   
   def index
     @user = User.paginate(:page => params[:page])
@@ -46,6 +47,12 @@ class UsersController < ApplicationController
     end
   end
   
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_path
+  end
+  
   private
   
     def authenticate
@@ -55,6 +62,11 @@ class UsersController < ApplicationController
     def correct_user
       @user = User.find(params[:id])
       redirect_to(root_path) unless current_user?(@user)
+    end
+    
+    def admin_user
+      user = User.find(params[:id])
+      redirect_to(root_path) unless (current_user.admin? && !current_user?(user))
     end
     
 
