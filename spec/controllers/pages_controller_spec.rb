22239714f542a996bca4fd34bc496414842a2a6e
Geoff Walmsley <mathsboy@gmail.com>
2012-01-04 14:32:45 +1300

    updated titles

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -4,7 +4,7 @@ describe PagesController do
   render_views
   
   before(:each) do
-    @base_title = "Ruby on Rails Tutorial"
+    @base_title = "Be Herd Prototype"
   end
 
   describe "GET 'home'" do
